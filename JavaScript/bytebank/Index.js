1fc98988fac4122d3296e1cd29991c4c065ff084
ThiagoYuri <thiagoyuri0109@gmail.com>
2021-12-07 15:13:21 -0300

    JavaScript: Programando a Orientação a Objeto > versão POG

---
+++ JavaScript/bytebank/Index.js
@@ -0,0 +1,12 @@
+const cliente1Nome = "Ricardo";
+const cliente1CPF = 111222333309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0 ;
+
+const cliente2Nome = "Alice"
+const cliente2CPF = 888222333309;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0 ;
+
+
+console.log(cliente1Nome)
