b096232d24a44b887fe89a054dea942b369de6b1
ThiagoYuri <thiagoyuri0109@gmail.com>
2021-12-07 15:37:09 -0300

    JavaScript: Programando a Orientação a Objeto > versão POO

--- JavaScript/bytebank/Index.js
+++ JavaScript/bytebank/Index.js
@@ -1,12 +1,23 @@
-const cliente1Nome = "Ricardo";
-const cliente1CPF = 111222333309;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0 ;
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
 
-const cliente2Nome = "Alice"
-const cliente2CPF = 888222333309;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0 ;
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Ricardo";
+cliente1.cpf = 111222333309;
+cliente1.agencia = 1001;
+cliente1.saldo = 0 ;
+
+cliente2.nome = "Alice"
+cliente2.cpf = 888222333309;
+cliente2.agencia = 1001;
+cliente2.saldo = 0 ;
 
 
-console.log(cliente1Nome)
+console.log(cliente1,cliente2)
+
