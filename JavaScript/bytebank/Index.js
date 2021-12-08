edb8edef4964b859e45de2b7179d85b0218c0ec6
ThiagoYuri <thiagoyuri0109@gmail.com>
2021-12-08 16:09:51 -0300

    JavaScript: Programando a Orientação a Objeto > Classe ContaCorrente criada com um metodo de sacar

--- JavaScript/bytebank/Index.js
+++ JavaScript/bytebank/Index.js
@@ -1,8 +1,17 @@
 class Cliente{
     nome;
     cpf;
+}
+
+
+class ContaCorrente{
     agencia;
-    saldo;
+    saldo;    
+
+    sacar (valor){
+        if(this.saldo>= valor )
+        this.saldo = this.saldo-valor;        
+    }
 }
 
 const cliente1 = new Cliente();
@@ -10,14 +19,20 @@ const cliente2 = new Cliente();
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 111222333309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0 ;
 
 cliente2.nome = "Alice"
 cliente2.cpf = 888222333309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0 ;
 
 
-console.log(cliente1,cliente2)
+const contaCorrenteRicarto = new ContaCorrente();
+contaCorrenteRicarto.saldo = 0;
+contaCorrenteRicarto.agencia =1001;
 
+console.log(contaCorrenteRicarto.saldo);
+contaCorrenteRicarto.saldo = 100;
+console.log(contaCorrenteRicarto.saldo);
+contaCorrenteRicarto.sacar(16);
+console.log(contaCorrenteRicarto.saldo);
+
+
+console.log(cliente1,cliente2);
