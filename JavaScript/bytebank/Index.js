93b6ea2c2de4490d439de1c856794be7a7fd27b3
ThiagoYuri <thiagoyuri0109@gmail.com>
2021-12-13 16:08:21 -0300

    JavaScript: Programando a Orientação a Objeto > Atualizando Index.js

--- JavaScript/bytebank/Index.js
+++ JavaScript/bytebank/Index.js
@@ -1,28 +1,17 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-
-
 //Corrente1
-const contaCorrenteRicarto = new ContaCorrente();
-contaCorrenteRicarto.agencia =1001;
-contaCorrenteRicarto.depositar(200)
-contaCorrenteRicarto.sacar(17);
-const cli1 = new Cliente();
-cli1.nome = "Ricardo";
-cli1.cpf = 111222333309;
-contaCorrenteRicarto.cliente = (cli1);
+
+const cli1 = new Cliente("Ricardo",111222333309);
+const contaCorrenteRicarto = new ContaCorrente(1500,cli1);
 
 //Corrente2
-const contaCorrenteAlice = new ContaCorrente();
-contaCorrenteAlice.agencia =1003;
-contaCorrenteAlice.depositar(500)
-contaCorrenteAlice.sacar(17);
-const cli2 = new Cliente();
-cli2.nome = "Alice";
-cli2.cpf = 111222209;
+const cli2 = new Cliente("Alice",111222209);
+const contaCorrenteAlice = new ContaCorrente(1003,cli2);
+contaCorrenteAlice.depositar(600);
 contaCorrenteAlice.cliente=(cli2);
 
 contaCorrenteAlice.transferir(200,contaCorrenteRicarto)
 console.log(contaCorrenteAlice);
-console.log(contaCorrenteAlice.saldo);
+console.log(ContaCorrente.numeroDeContas);
