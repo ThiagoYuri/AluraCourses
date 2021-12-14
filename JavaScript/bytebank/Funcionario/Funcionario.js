d20daee2303cbc3d6632cbf12fcd4d036581f086
ThiagoYuri <thiagoyuri0109@gmail.com>
2021-12-14 16:47:28 -0300

    deletando arquivos duplicados

--- JavaScript/bytebank/Funcionario/Funcionario.js
+++ JavaScript/bytebank/Funcionario/Funcionario.js
@@ -3,8 +3,13 @@ export class Funcionario{
         this._nome = nome;
         this._salario = salario;
         this._cpf = cpf;
+
         this._bonificacao = 1;
+        this._senha;
     }
 
+    cadastrarSenha(senha){
+    this._senha = senha;
+    }
 
 }
