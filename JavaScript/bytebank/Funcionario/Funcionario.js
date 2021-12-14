d18c6e81a02b6c7cf2ee51a0ab0893c61abd8e4c
ThiagoYuri <thiagoyuri0109@gmail.com>
2021-12-14 16:41:57 -0300

    JavaScript: Interfaces e Herança em Orientação a Objetos > Classes Funcionarios

---
+++ JavaScript/bytebank/Funcionario/Funcionario.js
@@ -0,0 +1,10 @@
+export class Funcionario{
+    constructor(nome,salario,cpf){
+        this._nome = nome;
+        this._salario = salario;
+        this._cpf = cpf;
+        this._bonificacao = 1;
+    }
+
+
+}
