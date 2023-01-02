3ef48810aa7f9b6bbae382b7bd4319d2ca9d535c
ThiagoYuri <thiagoyuri0109@gmail.com>
2023-01-02 09:42:09 -0300

    Update: FilmesAPI

---
+++ C-Sharp/FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,22 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace FilmesAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : Controller
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionarFilme([FromBody]Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
---
+++ C-Sharp/FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,11 @@
+﻿namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        public string Titulo { get; set; }
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        public int Duracao { get; set; }
+
+    }
+}
