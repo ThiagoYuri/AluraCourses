e29184736920148fb4ef6c1cb52649e18e6bbea7
ThiagoYuri <thiagoyuri0109@gmail.com>
2023-03-16 22:53:52 -0300

    aluramidi: add for

--- JavaScript/aluramidi/main.js
+++ JavaScript/aluramidi/main.js
@@ -6,16 +6,13 @@ function tocaSom(idElementAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador =0;
-//enquanto
-while(contador < listaDeTeclas.length){
+//para
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const tecla = listaDeTeclas[contador];
-
     var instrumento = listaDeTeclas[contador].classList[1];
-
     const idAudio = `#som_${instrumento}`;
+    
     tecla.onclick = function(){
         tocaSom(idAudio);
     }
-    contador = contador +1;
 }
