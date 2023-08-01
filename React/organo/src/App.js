b9259e75c5e38c81fac198462b4937e980586693
ThiagoYuri <thiagoyuri0109@gmail.com>
2023-08-01 11:10:30 -0300

    Project react

---
+++ React/organo/src/App.js
@@ -0,0 +1,15 @@
+
+import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto/CampoTexto';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      <CampoTexto></CampoTexto>
+    </div>
+  );
+}
+
+export default App;
